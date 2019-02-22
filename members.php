409c37528daad781159c9861e7da6aa0b950e285
Wen-Ling Chen <kuragechan14@gmail.com>
2019-02-22 23:35:30 +0800

    Change the title of pages. MBL@NCTU

--- members.php
+++ members.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>MBL Test</title>
+	<title>MBL@NCTU</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!– Bootstrap –>
